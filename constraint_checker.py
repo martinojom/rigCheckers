39be9a9491b80d0a208bc6c546ca5c9711c6e63d
martinojom <martinojom28@gmail.com>
2023-09-13 17:13:08 +0200

    Update constraint_checker.py
    python my_name

--- constraint_checker.py
+++ constraint_checker.py
@@ -1,3 +1,7 @@
 # constraint checker
 
-print("hello martin")
+my_name = "python"
+if len(my_name) != 4:
+  print(f"{my_name} is the your name"
+else:
+  print("this is not the length of my {my_name}")
